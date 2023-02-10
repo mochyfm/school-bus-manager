109a8ea37dbeeee8e2e831d2817c195ee7e2b34a
Moisés Fernández Muiña <mochyfm@gmail.com>
2023-02-10 12:01:59 +0000

    Feature: Finnished the Clients Section

--- src/Components/Info/StudentInfo/StudentInfo.tsx
+++ src/Components/Info/StudentInfo/StudentInfo.tsx
@@ -9,11 +9,14 @@ const StudentInfo = (props: Student) => {
   return (
     <div className="student">
         <div className="studentTitle">Estudiante</div>
+      <div className="studentInfoGroup">
         <div className="studentInfo">
-            <span className="studentDataLabel"><span className="studentLabel">Nombre</span> {name}</span>
-            <span className="studentDataLabel"><span className="studentLabel">Ruta asignada</span> {service.type}</span>
+            <span className="studentDataLabel"><span className="studentLabel">Nombre:</span> {name}</span>
+            <span className="studentDataLabel"><span className="studentLabel">Ruta asignada:</span> {service.type}</span>
         </div>
         <Link to={`/studentInfo/${id}`} className="link viewButton">Ver Estudiante</Link>
+        <div className="studentFooter"/>
+      </div>
     </div>
   )
 }
